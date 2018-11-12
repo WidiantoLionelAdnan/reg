618907b9005e622f2d499059b85d12562413549a
Petrik Lajos Szg <info@petrik.hu>
2018-11-12 15:01:36 +0100

    test

---
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace RegisztracioAlkalmazas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.szulido = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.HobbiListBox = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.UjHobbiTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MentesButton = new System.Windows.Forms.Button();
+            this.BetoltesButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Szül.idő:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nem:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(53, 6);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // szulido
+            // 
+            this.szulido.Location = new System.Drawing.Point(53, 40);
+            this.szulido.Name = "szulido";
+            this.szulido.Size = new System.Drawing.Size(100, 20);
+            this.szulido.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(58, 70);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "F";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(95, 70);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(33, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "N";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kedvenc hobbi:";
+            // 
+            // HobbiListBox
+            // 
+            this.HobbiListBox.FormattingEnabled = true;
+            this.HobbiListBox.Location = new System.Drawing.Point(239, 22);
+            this.HobbiListBox.Name = "HobbiListBox";
+            this.HobbiListBox.Size = new System.Drawing.Size(120, 95);
+            this.HobbiListBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(204, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Új hobbi:";
+            // 
+            // UjHobbiTextBox
+            // 
+            this.UjHobbiTextBox.Location = new System.Drawing.Point(259, 141);
+            this.UjHobbiTextBox.Name = "UjHobbiTextBox";
+            this.UjHobbiTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UjHobbiTextBox.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Hozzáad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // MentesButton
+            // 
+            this.MentesButton.Location = new System.Drawing.Point(198, 265);
+            this.MentesButton.Name = "MentesButton";
+            this.MentesButton.Size = new System.Drawing.Size(75, 23);
+            this.MentesButton.TabIndex = 12;
+            this.MentesButton.Text = "Mentés";
+            this.MentesButton.UseVisualStyleBackColor = true;
+            // 
+            // BetoltesButton
+            // 
+            this.BetoltesButton.Location = new System.Drawing.Point(284, 265);
+            this.BetoltesButton.Name = "BetoltesButton";
+            this.BetoltesButton.Size = new System.Drawing.Size(75, 23);
+            this.BetoltesButton.TabIndex = 13;
+            this.BetoltesButton.Text = "Betöltés";
+            this.BetoltesButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Név:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 311);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BetoltesButton);
+            this.Controls.Add(this.MentesButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UjHobbiTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.HobbiListBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.szulido);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox szulido;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox HobbiListBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox UjHobbiTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button MentesButton;
+        private System.Windows.Forms.Button BetoltesButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RegisztracioAlkalmazas/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RegisztracioAlkalmazas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            HobbiListBox.Items.Add("Úszás");
+            HobbiListBox.Items.Add("Horgászat");
+            HobbiListBox.Items.Add("Futás");
+
+            BetoltesButton.Click += (sender, e) =>
+            {
+                try
+                {
+                    string tartalom = Beolvasas();
+                    MessageBox.Show(tartalom);
+                }
+                catch(FileNotFoundException fnfe)
+                {
+                    MessageBox.Show("A fájl nem található");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Hiba");
+                }
+            };
+
+            MentesButton.Click += (sender, e) =>
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                {
+                    return;
+                }
+                string FileName = saveFileDialog1.FileName;
+            };
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(UjHobbiTextBox.Text))
+            {
+                HobbiListBox.Items.Add(UjHobbiTextBox.Text);
+            }
+        }
+
+        private string Beolvasas()
+        {
+            string FileName = nameTextBox.Text;
+            if (string.IsNullOrWhiteSpace(FileName))
+            {
+                
+                throw new Exception("A fájlnév üres");
+            }
+            return File.ReadAllText(FileName);
+        }
+    }
+}
